e61e7895f124d09dd20432975c6e2228edba406d
luishens01 <luis.hens@atos.net>
2018-12-03 10:28:53 +0100

    Change login and update. Now Admin user can change all data of all users. Nom admin user can change their own data, cannot save password empty and cannot assign admin role

--- routes/login.rb
+++ routes/login.rb
@@ -3,51 +3,34 @@ post "/login" do
 	puts "#{params[:username]}"
 	puts "#{params[:password]}"
 
-
 	@user = User.find_by_username(params[:username])
-
 	@typed_password = "#{params[:password]}"
-
 	puts "typed_password"
 	puts @typed_password
 	pwd = Digest::SHA1.hexdigest @typed_password.to_s
 	puts "this is the login encrypted password"
 	puts pwd
-	#puts "paco encrypted"
-	#pwd_2 = Digest::SHA1.hexdigest 'paco'
-	#puts pwd_2
-
-
-	@user['password']
-
+	puts "this is the DDBB encrypted password"
+	puts @user['password']
 
 	if pwd == @user['password']	
 
 		t_now = Time.now
 		puts t_now
 		t_60 = t_now + 3600			
-
-		#payload = { username: "#{params[:username]}", password: @user['email'], exp: Time.now.to_i + 60 * 60, iat: Time.now.to_i}
-		payload = { username: "#{params[:username]}", password: @user['email'], login_time: t_now, expiration_time: t_60}
+		payload = { username: "#{params[:username]}", email: @user['email'], login_time: t_now, expiration_time: t_60}
 		token = JWT.encode(payload, SECRET,'HS256')
-
 		json_token = JSON.generate("token"=>token )
 		puts json_token
 
         role = @user['role']
 		endpoints = Role.where(role: role ).select("endpoint", "verbs").all
-		#ep = endpoints.to_json		
+
 		ep = JSON.parse (endpoints.to_json)
 		puts ep
 		
 		json_output = JSON.pretty_generate [{"token"=>token},{"endpoints"=>ep}]
 		puts json_output
-
-		t_now = Time.now
-		puts t_now
-		t_60 = t_now + 3600
-		puts t_60
-
 
 		return 200, json_output
 
--- routes/update.rb
+++ routes/update.rb
@@ -61,20 +61,31 @@ post '/users/:username/update' do
 
     if @user['role'] == "admin"
 
-        @user_for_status = User.find_by_username( username_for_status ) 
-
-        if @user_for_status
-
-            new_status_body = JSON.parse(request.body.read)
-            puts new_status_body
-            new_status = new_status_body['status']
+        @user_for_update = User.find_by_username( username_for_status ) 
+        if @user_for_update
+            new_body = JSON.parse(request.body.read)
+            puts new_body
+            new_status = new_body['status']
+            new_role = new_body['role']
+            new_password = new_body['password']
+            new_email = new_body['email']
             puts new_status
+            puts new_role
 
-            puts @user_for_status   
-            user_for_status_2 = User.find_by_username(username_for_status).update_column(:status, new_status)
-            #user_for_status_2.update_attribute(:status, new_status)
+            puts new_password
+            new_password_encrypted = Digest::SHA1.hexdigest new_password
+            puts "new password encrypted"
+            puts new_password_encrypted            
 
-            return 200, "User status updated"
+            puts new_email
+            puts @user_for_update   
+
+            user_for_update_2 = User.find_by_username(username_for_update).update_column(:status, new_status)
+            user_for_update_3 = User.find_by_username(username_for_update).update_column(:role, new_role)
+            user_for_update_4 = User.find_by_username(username_for_update).update_column(:password, new_password_encrypted)
+            user_for_update_5 = User.find_by_username(username_for_update).update_column(:email, new_email)
+
+            return 200, "User info updated"
         end
     else 
         @user_for_update = User.find_by_username( username_for_status ) 
@@ -87,13 +98,39 @@ post '/users/:username/update' do
             new_email = new_body['email']
             puts new_status
             puts new_role
+
             puts new_password
+            #new_password_encrypted = Digest::SHA1.hexdigest new_password
+            #puts "new password encrypted"
+            #puts new_password_encrypted            
+
             puts new_email
             puts @user_for_update   
+
             user_for_update_2 = User.find_by_username(username_for_update).update_column(:status, new_status)
-            user_for_update_3 = User.find_by_username(username_for_update).update_column(:role, new_role)
-            user_for_update_4 = User.find_by_username(username_for_update).update_column(:password, new_password)
+            #user_for_update_3 = User.find_by_username(username_for_update).update_column(:role, new_role)
+            #user_for_update_4 = User.find_by_username(username_for_update).update_column(:password, new_password_encrypted)
             user_for_update_5 = User.find_by_username(username_for_update).update_column(:email, new_email)
+
+            if new_role == "admin"  
+                msg="This user cannot select the admin role"
+                return 409, msg.to_json
+            else
+                user_for_update_3 = User.find_by_username(username_for_update).update_column(:role, new_role)
+            end    
+
+            if new_password == ""
+                msg="This user cannot let the password empty"
+                return 409, msg.to_json
+            else
+                puts new_password
+                new_password_encrypted = Digest::SHA1.hexdigest new_password
+                puts "new password encrypted"
+                puts new_password_encrypted  
+                user_for_update_4 = User.find_by_username(username_for_update).update_column(:password, new_password_encrypted) 
+            end    
+
+
             return 200, "User info updated"
         else 
             msg="unregistered user"
